b590053f9b205419856bccd5f33f7ac5a7cffb92
Fi Dunnington <fi.dunnington@gmail.com>
2023-03-14 10:11:47 +0100

    Homepage added

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,3 +1,12 @@
 import React from "react";
 
-function Home()
+function Homepage() {
+    return (
+        <div>
+            <h1>Fi Dunnington</h1>
+             <p>Ipsum lorem etc etc</p>
+        </div>
+    );
+}
+
+export default Homepage;
