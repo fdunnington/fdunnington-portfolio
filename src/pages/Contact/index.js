75f6b38b4c4f694538d9489882b42cb1c181b5aa
Fi Dunnington <fi.dunnington@gmail.com>
2023-03-16 16:31:40 +0100

    trying to fix homepage issue

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Container from "../../components/Container";
 import Row from "../../components/Row";
-import Card from "../../components/Card"
+import Card from "../../components/Card";
+import Grid from "../../components/Grid";
 import "./style.css";
 
 function Contact() {
@@ -9,35 +10,37 @@ function Contact() {
     <div>
       <Container >
         <Row >
-          <Card >
-            <div class="info-item">
-              <i class="fa fa-map-marker"></i>
-              <div>
-                <h4 class="strong">Location</h4>
-                <p>Cornwall, UK</p>
+          <Grid>
+            <Card >
+              <div class="info-item">
+                <i class="fa fa-map-marker"></i>
+                <div>
+                  <h4 class="strong">Location</h4>
+                  <p>Cornwall, UK</p>
+                </div>
               </div>
-            </div>
-          </ Card>
+            </ Card>
 
-          <Card >
-            <div class="info-item">
-              <i class="fa fa-envelope"></i>
-              <div>
-                <h4 class="strong">Email</h4>
-                <p><a href="mailto:fi@fidunnington.com">fi@fidunnington.com</a></p>
+            <Card >
+              <div class="info-item">
+                <i class="fa fa-envelope"></i>
+                <div>
+                  <h4 class="strong">Email</h4>
+                  <p><a href="mailto:fi@fidunnington.com">fi@fidunnington.com</a></p>
+                </div>
               </div>
-            </div>
-          </ Card>
+            </ Card>
 
-          <Card >
-            <div class="info-item">
-              <i class="fa fa-phone"></i>
-              <div>
-                <h4 class="strong">Call/WhatsApp</h4>
-                <p><a href="tel:00447481844007">0044 7481 844 007</a></p>
+            <Card >
+              <div class="info-item">
+                <i class="fa fa-phone"></i>
+                <div>
+                  <h4 class="strong">Call/WhatsApp</h4>
+                  <p><a href="tel:00447481844007">0044 7481 844 007</a></p>
+                </div>
               </div>
-            </div>
-          </ Card>
+            </ Card>
+          </Grid>
         </ Row>
       </Container >
     </div>
