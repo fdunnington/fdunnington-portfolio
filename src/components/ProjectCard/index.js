eeb4c08206780723c7ed44a43e7bbd6fd12f177f
Fi Dunnington <fi.dunnington@gmail.com>
2023-03-17 14:08:19 +0100

    no changes

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -14,10 +14,10 @@ function ProjectCard(props) {
             <h3 className="project-name">{props.name}</h3>
           </li>
           <li>
+            <span>
             <a href={props.link} target="_blank"><i class="fa fa-link" id="link-icon"></i></a> 
-          </li>
-          <li>
-            <a href={props.github} target="_blank"><strong>Check out the github repo </strong></a>
+            <a href={props.github} target="_blank"><i class="fa fa-github"></i></a>
+            </span>
           </li>
         </ul>
       </div>
